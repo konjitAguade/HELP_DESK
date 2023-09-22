fa137595ed68a06bfc1e92f1564e25736b41059d
Abelti <abeltibbeshana@gmail.com>
2023-09-22 18:32:06 +0300

    starting-point

---
+++ routes/r-user.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const userRouter = 
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.post('/register', (req, res) => {
+    res.send('register page');
+});
+
+app.post('/login', (req, res) => {
+    res.send('login page');
+});
+//CRUD
+app.post('/request/create', (req, res) => {
+    res.send('request creation page');
+});
+
+app.get('/request/view', (req, res) => {
+    res.send('request view page');
+});
+
+app.put('/request/update', (req, res) => {
+    res.send('request update page');
+});
+
+app.delete('/request/resolve', (req, res) => {
+    res.send('request resolve page');
+});
+
+app.post('/feedback', (req, res) => {
+    res.send('feedback page');
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 3030;
+
+app.get("/", (req, res) => {
+  res.send("Here we go!");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Server listening on ${port}`);
+});
