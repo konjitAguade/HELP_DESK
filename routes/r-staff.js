d82218f7b777e7008f4a8ad2ced8456f77217ef3
konjit <konjitagu@gmail.com>
2023-10-04 18:19:34 +0300

    first

--- routes/r-staff.js
+++ routes/r-staff.js
@@ -5,11 +5,6 @@ const staffRouter =  express.Router();
 staffRouter.get('/', (req, res,next) => {
     res.send('home');
     next();
-//users/get-user-details
-Router.get('/get-user-details', (req, res)) 
-=> {
-    res.send("Get Request for users")
-}
 });
 
 
